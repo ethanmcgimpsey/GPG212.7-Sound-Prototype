15e7a889519e91ca611dedc6ac38d7442825cde1
MrMuskyHusky <1027613@student.sae.edu.au>
2023-07-28 00:44:40 +1000

    Update #3

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 using System.Linq;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    public List<QuestionAndAnswer> questionAndAnswers = new List<QuestionAndAnswer>();
-    public GameObject imageChoicePanelPrefab;
+    public List<Question> questionAndAnswers = new List<Question>();
     public int questionCounter = 0;
+    public Button[] currentButton;
 
-    [SerializeField] private QuestionAndAnswer currentQuestion;
+    [SerializeField] private Question currentQuestion;
 
     void Start()
     {
-        questionAndAnswers = questionAndAnswers.OrderBy(item => Random.Range(0f, 1f)).ToList(); //Shuffles the question list
         ResetRoom();
+    }
+
+    void Update()
+    {
+
     }
 
     // Reset everything in the room
@@ -56,59 +60,20 @@ public class GameController : MonoBehaviour
 
     }
 
-    public void OnAnswer(Texture2D selectedImage)
+    public void OnAnswer()
     {
-        if (currentQuestion.answerImages.Contains(selectedImage))
+        // DEBVUG LOG
+        // if (answerField.text.ToLower() == currentQuestion.answer.ToLower())
+        if (currentQuestion.answer = currentButton[questionCounter])
         {
             StartCoroutine(ExitRoom());
         }
     }
 
-    // Show image choices instead of text question
-    private void ShowImageChoices()
-    {
-        currentQuestion = questionAndAnswers[questionCounter++];
-
-        // Clear any existing image choice panels
-        var existingPanels = GameObject.FindGameObjectsWithTag("ImageChoicePanel");
-        foreach (var panel in existingPanels)
-        {
-            Destroy(panel);
-        }
-
-        // Find the parent container for image choices
-        GameObject choicesContainerParent = GameObject.Find("ImageChoiceContainerParent");
-        if (choicesContainerParent == null)
-        {
-            Debug.LogError("ImageChoiceContainerParent not found in the scene.");
-            return;
-        }
-
-        // Create image choice container (empty game object)
-        GameObject choicesContainer = new GameObject("ImageChoiceContainer");
-        choicesContainer.transform.SetParent(choicesContainerParent.transform, false);
-
-        // Create image choice panels for each answer option
-        foreach (var answerImage in currentQuestion.answerImages)
-        {
-            GameObject choicePanel = Instantiate(imageChoicePanelPrefab);
-            choicePanel.tag = "ImageChoicePanel";
-            choicePanel.transform.SetParent(choicesContainer.transform, false);
-
-            // Set the image of the Image component
-            var image = choicePanel.GetComponent<Image>();
-            Sprite sprite = Sprite.Create(answerImage, new Rect(0, 0, answerImage.width, answerImage.height), new Vector2(0.5f, 0.5f));
-            image.sprite = sprite;
-
-            var button = choicePanel.GetComponent<Button>();
-            button.onClick.AddListener(() => OnAnswer(answerImage));
-        }
-    }
-
     [System.Serializable]
-    public class QuestionAndAnswer
+    public class Question
     {
-        public string question;
-        public List<Texture2D> answerImages;
+        public GameObject currentQuestion;
+        public Button answer;
     }
 }
---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Question : MonoBehaviour
+{
+    public string question; //Creates question
+    public Button answer; //Creates Answer
+}
